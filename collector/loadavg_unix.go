35df3b86acc3d6edb04d219fdea12c2b814a4991
Johannes 'fish' Ziemke <github@freigeist.org>
2016-12-27 11:59:44 +0100

    Use go native loadavg implementation for all bsds

--- collector/loadavg_unix.go
+++ collector/loadavg_unix.go
@@ -1,34 +1,28 @@
-// Copyright 2015 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build darwin dragonfly netbsd openbsd solaris
+// +build darwin dragonfly freebsd netbsd openbsd solaris
 // +build !noloadavg
 
 package collector
 
 import (
-	"errors"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
-// #include <stdlib.h>
-import "C"
-
 func getLoad() ([]float64, error) {
-	var loadavg [3]C.double
-	samples := C.getloadavg(&loadavg[0], 3)
-	if samples > 0 {
-		return []float64{float64(loadavg[0]), float64(loadavg[1]), float64(loadavg[2])}, nil
-	} else {
-		return nil, errors.New("failed to get load average")
+	type loadavg struct {
+		load  [3]uint32
+		scale int
 	}
+	b, err := unix.SysctlRaw("vm.loadavg")
+	if err != nil {
+		return nil, err
+	}
+	load := *(*loadavg)(unsafe.Pointer((&b[0])))
+	scale := float64(load.scale)
+	return []float64{
+		float64(load.load[0]) / scale,
+		float64(load.load[1]) / scale,
+		float64(load.load[2]) / scale,
+	}, nil
 }
